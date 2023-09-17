23eb21d622737372fdf42a4c73c50623bced53f1
FerRamirez99 <fer.ramirez99>
2023-09-17 17:41:59 -0300

    Ajuestes a grafico con chart js y html, avance con css

--- js/script.js
+++ js/script.js
@@ -1,106 +1,140 @@
 //Importacion
 import { fetchApi } from "./fetch.js";
 
-//Variables cards
-let temp = [];
-let name = [];
-let hum = [];
-let est = [];
-let icon = [];
+//Variables grafico
+let chartInstance = null;
+const color1 = '#ED9E7C';
+const color2 = '#ACB9C3';
+const color3 = '#667B87'
 
-//Variables graficos
 let fechas = [];
 let promTemp = [];
+let senTemp = [];
 let maxTemp = [];
 let minTemp = [];
-let senTemp = [];
 
 async function showData() {
-    const clima = await fetchApi("https://api.gael.cloud/general/public/clima");
-    const climaGrafico = await fetchApi("https://archive-api.open-meteo.com/v1/archive?latitude=-33.4569&longitude=-70.6483&start_date=2023-08-07&end_date=2023-09-07&daily=temperature_2m_max,temperature_2m_min,temperature_2m_mean,apparent_temperature_mean&timezone=auto");
+    //Fecha actual
+    const currentDate = new Date();
+    const nintyDaysAgo = new Date(currentDate);
+    //Fecha de hace 60 dias/2 meses
+    nintyDaysAgo.setDate(nintyDaysAgo.getDate() - 90);
+    //Formatea fecha de "Sun Sep 17 2023 15:42:29 GMT-0300 (Chile Summer Time)" a "2023-07-19"
+    const formattedDate = nintyDaysAgo.toISOString().split('T')[0];
+    //Guarda elemento datepicker 1 en variable
+    const startDateInput = document.getElementById("dateStart");
+    //Pasa el valor al elemento
+    startDateInput.value = formattedDate;
 
-    //Pasar datos a los arrays de las cards
-    temp = clima.map(clima => clima.Temp);
-    name = clima.map(clima => clima.Estacion);
-    hum = clima.map(clima => clima.Humedad);
-    est = clima.map(clima => clima.Estado);
-    icon = clima.map(clima => clima.Icono);
+    //Pasar fecha actual a parte card
+    document.getElementById('today').innerHTML = currentDate.toISOString().split('T')[0];
 
-    //Escribir datos en el card
-    document.getElementById('cardTitle').innerHTML = name[31];
-    document.getElementById('cardTemp').innerHTML = `${temp[31]} °C`;
-    document.getElementById('cardHum').innerHTML = `<i class="fa-solid fa-droplet"></i> ${hum[31]}%`;
-    document.getElementById('cardEst').innerHTML =  est[31];
-    // const cardIcon = document.getElementById('cardIcon').src = icon[31];
-    // console.log(cardIcon);
+    //Capturar valores del datepicker
+    let startDate = new Date(document.getElementById("dateStart").value);
+    let endDate = new Date(document.getElementById("dateEnd").value);
+    //Formatear valores
+    let formattedStart = startDate.toISOString().split('T')[0];
+    let formattedEnd = endDate.toISOString().split('T')[0];
+    
+    //Llama al fetch para recoger datos
+    // const clima = await fetchApi("https://api.gael.cloud/general/public/clima");
+    const climaGrafico = await fetchApi(`https://archive-api.open-meteo.com/v1/archive?latitude=-33.4569&longitude=-70.6483&start_date=${formattedStart}&end_date=${formattedEnd}&daily=temperature_2m_max,temperature_2m_min,temperature_2m_mean,apparent_temperature_mean&timezone=auto`);
 
     //Pasar datos a los arrays del grafico
     fechas = climaGrafico.daily.time;
-    maxTemp = climaGrafico.daily.temperature_2m_max;
-    minTemp = climaGrafico.daily.temperature_2m_min;
     promTemp = climaGrafico.daily.temperature_2m_mean;
     senTemp = climaGrafico.daily.apparent_temperature_mean;
+    maxTemp = climaGrafico.daily.temperature_2m_max;
+    minTemp = climaGrafico.daily.temperature_2m_min;
 
-    //Codigo para crear grafico 1
-    const cdx = document.getElementById('myChart');
-    new Chart(cdx, {
-        type: 'line',
-        data: {
-            labels: fechas,
-            datasets: [
-                {
-                    label: "Temperatura Promedio",
-                    data: promTemp,
-                    borderColor: 'rgb(237, 158, 124)',
-                    tension: 0.1
+    //Codigo para crear grafico
+    if (chartInstance) {
+        chartInstance.data.labels = fechas;
+        chartInstance.data.datasets[0].data = promTemp;
+        chartInstance.data.datasets[0].backgroundColor = color2;
+        chartInstance.data.datasets[0].borderColor = color1;
+        chartInstance.update(); // Actualizar el gráfico con los nuevos datos
+    } else {
+        const cdx = document.getElementById('myChart');
+        chartInstance = new Chart(cdx, {
+            type: 'line',
+            data: {
+                labels: fechas,
+                datasets: [
+                    {
+                        label: "Temperatura Promedio",
+                        data: promTemp,
+                        borderColor: color1,
+                        backgroundColor:color1,
+                        tension: 0.1
+                    },
+                    
+                    {
+                        type: 'line',
+                        data: maxTemp,
+                        label: "T° Max",
+                        borderColor: color3,
+                        backgroundColor: color3,
+                        tension: 0.1,
+                        hidden: true
+                    },
+                    {
+                        type: 'line',
+                        data: minTemp,
+                        label: "T° Min",
+                        borderColor: color3,
+                        backgroundColor: color3,
+                        tension: 0.1,
+                        hidden: true
+                    },
+                    {
+                        type: 'bar',
+                        data: senTemp,
+                        label: "Sensación Térmica",
+                        borderColor: color2,
+                        backgroundColor: color2,
+                        tension: 0.1
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
                 },
-                {
-                    type: 'bar',
-                    label: 'Sensación Termica',
-                    data: senTemp,
-                    borderColor: 'rgb(172, 185, 195)'
-                },
-            ],
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: false,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: "Temperaturas históricas en Santiago",
+                        padding: {
+                            top: 20,
+                            bottom: 10,
+                        },
+                    },
+                    tooltip: {
+                        callbacks: {
+                            label: function (context) {
+                                let label = context.dataset.label || "";
+                                if (label) {
+                                    label += ": ";
+                                }
+                                if (context.parsed.y !== null) {
+                                    label += context.parsed.y + "°C";
+                                }
+                                return label;
+                            },
+                        },
+                    },
                 },
             },
-        },
-    });
-
-    //Codigo para crear grafico 2
-    const cdx1 = document.getElementById('myChart1');
-    new Chart(cdx1, {
-        type: 'line',
-        data: {
-            labels: fechas,
-            datasets: [
-                {
-                    label: 'Temperatura Maxima',
-                    data: maxTemp,
-                    borderColor: 'rgb(237, 158, 124)',
-                    fill: false,
-                    tension: 0.1
-                },
-                {
-                    type: 'line',
-                    label: 'Temperatura Minima',
-                    data: minTemp,
-                    borderColor: 'rgb(172, 185, 195)'
-                }
-            ]
-        }
-    })
+        });
+    }
 }
 
-showData();
+window.addEventListener("load", () => {
+    const generateButton = document.getElementById("btn-datePicker");
+    generateButton.addEventListener("click", showData);
+});
 
-// function dropdownSelect(){
-//     let d = document.getElementById('changePlace');
-//     let display = d.options[d.selectedIndex].text;
-//     document.getElementById('msg').innerHTML = display;
-// }
-// dropdownSelect();
+showData();
